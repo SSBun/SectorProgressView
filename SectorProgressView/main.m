f8b2ab40fb7f245e7f0a7592e00153f37066b5c7
蔡士林 <caishilin@babyfs.com>
2018-11-29 14:45:11 +0800

    Initial Commit

---
+++ SectorProgressView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SectorProgressView
+//
+//  Created by caishilin on 2018/11/29.
+//  Copyright © 2018 caishilin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
